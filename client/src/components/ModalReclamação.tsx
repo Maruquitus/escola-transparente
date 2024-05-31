ecbb2aff4ccb3c465ee4d662f4b25fb6abc7bbd3
Maruquitus <marcospauloda.s.s@gmail.com>
2024-05-31 10:33:40 -0300

    Correções na exibição das imagens

--- client/src/components/ModalReclamação.tsx
+++ client/src/components/ModalReclamação.tsx
@@ -12,8 +12,7 @@ export function ModalReclamação(props: {
   return (
     <div
       onClick={(event) => {
-        const modal = document.getElementById("modal");
-        if (!modal?.contains(event.target as Node)) {
+        if (!(event.target as Element).closest(".modal")) {
           props.setModalAberto(false);
         }
       }}
@@ -23,8 +22,8 @@ export function ModalReclamação(props: {
           : "opacity-0 pointer-events-none"
       } fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center transition-opacity duration-300 backdrop-blur-md z-10`}
     >
-      <div id="modal" className="md:mb-0 mb-16 md:w-4/5 md:mx-auto">
-        <h1 className="font-sans w-fit mx-auto font-semibold text-3xl bg-white rounded-lg shadow-md p-3">
+      <div className="md:mb-0 mb-16 md:w-4/5 md:mx-auto">
+        <h1 className="modal font-sans w-fit mx-auto font-semibold text-3xl bg-white rounded-lg shadow-md p-3">
           {props.reclamação?.fotos.length > 1
             ? "Imagens anexadas"
             : "Imagem anexada"}
@@ -33,7 +32,7 @@ export function ModalReclamação(props: {
         <div className="hidden md:flex justify-around space-x-4 w-full mt-2">
           {props.reclamação?.fotos.map((foto: string) => {
             return (
-              <div className="aspect-[10/11] w-full bg-white px-3 py-4 pb-8 shadow-md rounded-md">
+              <div className="aspect-[10/11] w-1/3 modal bg-white px-3 py-4 pb-8 shadow-md rounded-md">
                 <div className="bg-gray-300 rounded-lg w-full h-full relative">
                   <img
                     alt=""
@@ -45,7 +44,7 @@ export function ModalReclamação(props: {
             );
           })}
         </div>
-        <div className="flex md:hidden w-80 bg-white shadow-md px-3 py-4 mt-2 rounded-md">
+        <div className="flex md:hidden w-80 bg-white shadow-md px-3 py-4 mt-2 rounded-md modal">
           <Swiper
             className="p-2 w-full h-80 md:hidden rounded-lg"
             spaceBetween={10}
